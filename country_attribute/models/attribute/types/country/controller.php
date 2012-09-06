dc153a946aa7d3ac78574148df77e56622c9ebfe
david reid <zathrasorama@gmail.com>
2012-09-06 17:43:54 +0100

    Use the correct empty setting. Not sure how this got changed.

--- country_attribute/models/attribute/types/country/controller.php
+++ country_attribute/models/attribute/types/country/controller.php
@@ -7,7 +7,7 @@ class CountryAttributeTypeController extends DefaultAttributeTypeController
 	public function form() 
 	{
 		$this->set('fieldPostName', $this->field('value'));
-		$val = is_object($this->attributeValue) ? $this->getAttributeValue()->getValue() : 0;
+		$val = is_object($this->attributeValue) ? $this->getAttributeValue()->getValue() : '';
 		$this->set('fieldValue', $val);
 	}
 }
